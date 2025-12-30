9a48720abe34f1ddc6c7286d8126e7b30f5cc02d
yusun choi <tanks2438@outlook.kr>
2025-12-31 08:36:08 +0900

    input: Add non-blocking input function

--- include/tetris/input/input.h
+++ include/tetris/input/input.h
@@ -9,6 +9,7 @@ private:
 	block current_block;
 	board current_board;
 public:
+	static char get_async_input();
 	input(block current_block, board current_board, char current_order);
 	void get_user_input();
 	bool activate_block();
--- src/input/input.cpp
+++ src/input/input.cpp
@@ -1,10 +1,28 @@
 #include "input.h"
+#include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+using namespace std;
 
 input::input(block current_block, board current_board, char current_order) :current_block(current_block), current_board(current_board), current_order(current_order) {}
 
+char input::get_async_input()
+{
+	char input;
+	cin >> input;
+	return input;
+}
+
 void input::get_user_input()
 {
-	cin >> current_order;
+	chrono::seconds timeout(0);
+	char input = 'x';
+	future<char> future = async(get_async_input);
+	if (future.wait_for(timeout) == future_status::ready)
+		input = future.get();
+
+	current_order = input;
 }
 
 bool input::activate_block()
@@ -12,59 +30,115 @@ bool input::activate_block()
 	int x = current_block.get_x();
 	int y = current_block.get_y();
 	bool is_gameover = false;
+	bool is_block_move_stop = false;
 
 	switch (current_order)
 	{
 	case 'a':
 		if (current_board.gameover_check(1, -1, 0))
 		{
+			is_block_move_stop = true;
 			is_gameover = true;
 			break;
 		}
 		if (current_board.left_right_collision_check(1, -1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.down_collision_check(1, -1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.block_collision_check(1, -1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		current_block.x_move(-1);
 		current_block.y_move(1);
 		break;
 	case 'd':
 		if (current_board.gameover_check(1, 1, 0))
 		{
+			is_block_move_stop = true;
 			is_gameover = true;
 			break;
 		}
 		if (current_board.left_right_collision_check(1, 1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.down_collision_check(1, 1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.block_collision_check(1, 1, 0))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		current_block.x_move(1);
 		current_block.y_move(1);
 		break;
 	case 's':
 		if (current_board.gameover_check(1, 0, 90))
 		{
+			is_block_move_stop = true;
 			is_gameover = true;
 			break;
 		}
 		if (current_board.left_right_collision_check(1, 0, 90))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.down_collision_check(1, 0, 90))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		if (current_board.block_collision_check(1, 0, 90))
+		{
+			is_block_move_stop = true;
 			break;
+		}
 		current_block.rotate(90);
+		current_block.y_move(1);
+		break;
+	default:
+		if (current_board.gameover_check(1, 0, 0))
+		{
+			is_block_move_stop = true;
+			is_gameover = true;
+			break;
+		}
+		if (current_board.left_right_collision_check(1, 0, 0))
+		{
+			is_block_move_stop = true;
+			break;
+		}
+		if (current_board.down_collision_check(1, 0, 0))
+		{
+			is_block_move_stop = true;
+			break;
+		}
+		if (current_board.block_collision_check(1, 0, 0))
+		{
+			is_block_move_stop = true;
+			break;
+		}
 		current_block.y_move(1);
 		break;
 	}
 
-	if (is_gameover == false)
+	current_board.update(is_block_move_stop);
+	if (is_block_move_stop == true)
 	{
-		current_board.update();
+		current_block.init_block();
 	}
 	return is_gameover;
 }
