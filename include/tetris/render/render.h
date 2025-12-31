41f28635bfad0b380c7f9816c7fd835195922f83
yusun choi <tanks2438@outlook.kr>
2025-12-31 11:27:26 +0900

    render: Add color to block

--- include/tetris/render/render.h
+++ include/tetris/render/render.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <board/board.h>
+#include <windows.h>
 
 class render
 {
 private:
 	int start_y;
 	int start_x;
+	WORD default_console_color;
 public:
 	render(int start_y, int start_x);
 	void prompt(int x, int y);
 	void print_plate();
 	void sleep(int seconds);
+	void set_color(WORD color);
+	void print_color_by_number(int num);
 };
--- src/render/render.cpp
+++ src/render/render.cpp
@@ -5,7 +5,13 @@
 #include <windows.h>
 using namespace std;
 
-render::render(int start_y, int start_x) :start_y(start_y), start_x(start_x) {}
+render::render(int start_y, int start_x) :start_y(start_y), start_x(start_x)
+{
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	HANDLE h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(h_console, &info);
+	default_console_color = info.wAttributes;
+}
 
 void render::prompt(int y, int x)
 {
@@ -27,7 +33,7 @@ void render::print_plate()
 		cout << '|';
 		for (int x = 0; x < plate_x; ++x)
 		{
-			cout << current_board.get_plate_num(y, x);
+			print_color_by_number(current_board.get_plate_num(y, x));
 		}
 		cout << "|\n";
 	}
@@ -42,4 +48,56 @@ void render::print_plate()
 void render::sleep(int seconds)
 {
 	std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+void render::set_color(WORD color)
+{
+	HANDLE h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(h_console, color);
+}
+
+void render::print_color_by_number(int num)
+{
+	switch (num)
+	{
+	case 0:
+		cout << num;
+		break;
+	case 1:
+	case 8:
+		set_color(FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 2:
+	case 9:
+		set_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 3:
+	case 10:
+		set_color(FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 4:
+	case 11:
+		set_color(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 5:
+	case 12:
+		set_color(FOREGROUND_RED | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 6:
+	case 13:
+		set_color(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	case 7:
+	case 14:
+		set_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		cout << num;
+		break;
+	}
+	set_color(default_console_color);
 }
