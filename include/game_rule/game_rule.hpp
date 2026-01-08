ac61c95e86f4d17793a4ca77349ca0e30159a211
GimunKim <gimun.kim00@gmail.com>
2026-01-08 12:30:42 +0900

    [Refactor] Migrate user input processing from Board to RuleEngine

--- include/game_rule/game_rule.hpp
+++ include/game_rule/game_rule.hpp
@@ -9,9 +9,12 @@ class RuleEngine
     int time_for_level_up[11];
     int level_game_time;
     int current_level;
+    bool enable_kick;
+    Board& board;
 
     public:
-    RuleEngine();
+    RuleEngine(Board& board);
+    void process(int user_input);
     bool is_game_over(const uint16_t* board);
     int update_score(Board& board);
     bool time_and_level_update();
