a20653e869030c7f3ffaedee87c3be5ec84fc031
GimunKim <gimun.kim00@gmail.com>
2026-01-10 14:09:19 +0900

    [Enhance] add game modes: BLITZ, FortyLines

--- include/game_rule/rule_blitz.hpp
+++ include/game_rule/rule_blitz.hpp
@@ -1,18 +1,17 @@
-#ifndef __RULE_ZEN_HPP__
-#define __RULE_ZEN_HPP__
+#ifndef __GAME_RULE_HPP__
+#define __GAME_RULE_HPP__
 
 #include "game_rule/rule.hpp"
 #include "board/board.hpp"
 
-class ZEN : public GameRule
+class BLITZ : public GameRule
 {
     private:
-    int time_for_level_up[11];
-    int level_game_time;
-    int current_level;
+    int time;
+    int score;
 
     public:
-    ZEN(Board& board);
+    BLITZ(Board& board);
     bool is_game_clear() override;
     bool is_game_over() override;
     int update_score() override;
--- include/game_rule/rule_forty_lines.hpp
+++ include/game_rule/rule_forty_lines.hpp
@@ -1,18 +1,16 @@
-#ifndef __RULE_ZEN_HPP__
-#define __RULE_ZEN_HPP__
+#ifndef __RULE_FORTY_LINES_HPP__
+#define __RULE_FORTY_LINES_HPP__
 
 #include "game_rule/rule.hpp"
 #include "board/board.hpp"
 
-class ZEN : public GameRule
+class FortyLines : public GameRule
 {
     private:
-    int time_for_level_up[11];
-    int level_game_time;
-    int current_level;
+    int complete_lines;
 
     public:
-    ZEN(Board& board);
+    FortyLines(Board& board);
     bool is_game_clear() override;
     bool is_game_over() override;
     int update_score() override;
