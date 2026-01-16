d0b0c5b5ed64f2634872e8433362bf38bf2f0388
Choi Yusun <tanks2438@outlook.kr>
2026-01-16 15:59:26 +0900

    [Refact] Add test to util and board
    Signed-off-by: Choi Yusun <tanks2438@outlook.kr>

---
+++ test/core/board/board_test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "board/board.hpp"
+
+TEST(BoardTest, SpawnMino)
+{
+    Board board;
+    board.spawn_mino(3);
+       
+    auto [r, c] = board.get_active_mino_pos();
+    int rotation =  board.get_active_mino_rotation();
+    int type = board.get_active_mino_type();
+
+    EXPECT_EQ(r, 0);
+    EXPECT_EQ(c, 3);
+    EXPECT_EQ(rotation, 0);
+    EXPECT_EQ(type, 3);
+}
+
+TEST(BoardTest, MoveMino)
+{
+    Board board;
+    board.spawn_mino(3);
+
+    board.move_active_mino(5, 5, 3, FIX_IF_FAIL);
+       
+    auto [r, c] = board.get_active_mino_pos();
+    int rotation =  board.get_active_mino_rotation();
+    int type = board.get_active_mino_type();
+
+    EXPECT_EQ(r, 5);
+    EXPECT_EQ(c, 5);
+    EXPECT_EQ(rotation, 3);
+    EXPECT_EQ(type, 3);
+}
