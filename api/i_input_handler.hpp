e017f36419e716431f1e83291f3b31f5babd1046
GimunKim <gimun.kim00@gmail.com>
2026-01-16 13:10:09 +0900

    [Feat] add arrow key handling

--- api/i_input_handler.hpp
+++ api/i_input_handler.hpp
@@ -1,6 +1,14 @@
 #ifndef __I_INPUT_HANDLER_HPP__
 #define __I_INPUT_HANDLER_HPP__
 
+enum Arrow
+{
+  UP = 1000,
+  DOWN,
+  LEFT,
+  RIGHT
+};
+
 class IInputHandler
 {
   public:
@@ -8,7 +16,7 @@ class IInputHandler
      * @brief 버퍼로부터 문자열 입력 받기
      * @return 입력 없을 때 "\0", 입력 있을 때 해당 문자열 반환
      */
-    virtual char scan() = 0;
+    virtual int scan() = 0;
 
     /**
      * @brief 소멸자
