86544df7e93d2becec736cc71cf6c0ea973a9ab4
남상엽 <spearoad15@gmail.com>
2026-03-10 16:18:04 +0900

    fix : Window Single 화면깨짐 수정

--- platform/window/source/render/window_renderer.cpp
+++ platform/window/source/render/window_renderer.cpp
@@ -2,11 +2,35 @@
 
 using namespace std;
 
-const int LOGO_X = 20;
-const int BOARD_X = 35;
+const int LOGO_X = 35;
+const int BOARD_X = 55;
 const int BOARD_Y = 7;
-const int LEFT_X = 4;
+const int LEFT_X = 0;
 const int MIDDLE_X = 80;
+
+const int BOARD_START_Y = 7;
+const int MY_TOTAL_WIDTH = 45;
+const int MY_TOTAL_HEIGHT = 30;
+const int ENEMY_BOARD_WIDTH = 20;
+const int ENEMY_BOARD_HEIGHT = 30;
+const int BOARD_WIDTH = 22;
+const int BOARD_HEIGHT = 21;
+
+const int BOX_WIDTH = 10;
+const int BOX_HEIGHT = 6;
+
+const int MARGIN = 1;
+const int GAP_X = 3;
+
+const int HOLD_Y = BOARD_START_Y;
+const int SCORE_Y = MARGIN * 2 + BOARD_START_Y + BOX_HEIGHT;
+const int LV_Y = MARGIN * 4 + BOARD_START_Y + BOX_HEIGHT * 2;
+const int TIMER_Y = BOARD_START_Y;
+const int NEXT_Y = BOARD_START_Y + BOX_HEIGHT / 2 + MARGIN;
+
+const int LEFT_BOX_X = MARGIN + LEFT_X;
+const int RIGHT_BOX_X = BOX_WIDTH + BOARD_WIDTH + MARGIN * 2 + LEFT_X;
+const int MY_BOARD_X = MARGIN * 3 + BOX_WIDTH + LEFT_X;
 
 WindowRenderer::WindowRenderer(Setting* a1, IPlatformRenderer* a2, ColorPicker a3, TextRenderer a4,
                                BoxRenderer a5, BlockRenderer a6, ShadowMaker a7)
@@ -23,15 +47,15 @@ void WindowRenderer::render_timer(int totalSec)
     string sec_str =
         to_string(min / 10) + to_string(min % 10) + ":" + to_string(sec / 10) + to_string(sec % 10);
 
-    platform_renderer->set_cursor(MIDDLE_X + 2, 4);
-    platform_renderer->print_s(sec_str, Color::COMMENT);
+    platform_renderer->set_cursor(RIGHT_BOX_X + 2, 8);
+    platform_renderer->print_s(sec_str, Color::COMMENT, Color::PANEL);
 }
 
 void WindowRenderer::render_clear() { platform_renderer->clear(); }
 void WindowRenderer::render_next_block(const int* tetrominoArray)
 {
     for (size_t i = 0; i < 3; ++i) {
-        int x = MIDDLE_X + 1, y = 11;
+        int x = RIGHT_BOX_X + 1, y = 15;
 
         Tetromino m1;
         Tetromino m2;
@@ -49,33 +73,43 @@ void WindowRenderer::render_next_block(const int* tetrominoArray)
 
 void WindowRenderer::render_hold(const Tetromino& tetromino)
 {
-    block_renderer.render_mino_pattern({5, 11}, tetromino, Color::PANEL);
+    block_renderer.render_mino_pattern({LEFT_BOX_X + 2, 9}, tetromino, Color::PANEL);
 }
 
 void WindowRenderer::render_background()
 {
     fflush(stdout);
-    text_renderer.draw_logo({LOGO_X, 1});
-    box_renderer.draw_box({LEFT_X, 9}, 9, 6, "[HOLD]", Color::GREEN, Color::PANEL);
-    box_renderer.draw_box({MIDDLE_X, 9}, 9, 17, "[NEXT]", Color::PURPLE, Color::PANEL);
-    box_renderer.draw_box({LEFT_X, 16}, 9, 4, "[SCORE]", Color::CYAN, Color::PANEL);
-    box_renderer.draw_box({LEFT_X, 21}, 9, 4, "[LV]", Color::CYAN, Color::PANEL);
-    box_renderer.draw_box({MIDDLE_X, 2}, 9, 4, "[TIME]", Color::COMMENT, Color::PANEL);
-    box_renderer.draw_line({BOARD_X, BOARD_Y}, 22, 21, Color::FOREGROUND);
+    text_renderer.draw_logo({LEFT_X, 1});
+    box_renderer.draw_box({LEFT_BOX_X, HOLD_Y}, BOX_WIDTH, BOX_HEIGHT, "[HOLD]", Color::GREEN,
+                          Color::PANEL);
+    box_renderer.draw_box({LEFT_BOX_X, SCORE_Y}, BOX_WIDTH, BOX_HEIGHT, "[SCORE]", Color::CYAN,
+                          Color::PANEL);
+    box_renderer.draw_box({LEFT_BOX_X, LV_Y}, BOX_WIDTH, BOX_HEIGHT, "[LV]", Color::CYAN,
+                          Color::PANEL);
+
+    // right box
+    box_renderer.draw_box({RIGHT_BOX_X, TIMER_Y}, BOX_WIDTH, BOX_HEIGHT / 2, "[TIME]",
+                          Color::COMMENT, Color::PANEL);
+    box_renderer.draw_box({RIGHT_BOX_X, NEXT_Y}, BOX_WIDTH, BOX_HEIGHT * 3, "[NEXT]", Color::PURPLE,
+                          Color::PANEL);
+
+    // my board
+    box_renderer.draw_line({MY_BOARD_X, BOARD_START_Y}, BOARD_WIDTH, BOARD_HEIGHT,
+                           Color::FOREGROUND);
 }
 
 void WindowRenderer::render_score(int score)
 {
     string score_str = to_string(score);
-    platform_renderer->set_cursor(12 - score_str.size(), 18);
-    platform_renderer->print_s(to_string(score), Color::CYAN);
+    platform_renderer->set_cursor(LEFT_BOX_X + 8 - (int) score_str.size(), SCORE_Y + 3);
+    platform_renderer->print_s(to_string(score), Color::CYAN, Color::PANEL);
 }
 
 void WindowRenderer::render_level(int level)
 {
     string level_str = to_string(level);
-    platform_renderer->set_cursor(12 - level_str.size(), 23);
-    platform_renderer->print_s(level_str, Color::CYAN);
+    text_renderer.print_big_string({LEFT_BOX_X + 3, LV_Y + 1}, level_str, Color::CYAN,
+                                   Color::PANEL);
 }
 
 // void WindowRenderer::render_game_over()
@@ -112,7 +146,7 @@ void WindowRenderer::render_board(const Board& board, const Tetromino& tetromino
     if (setting->shadow_on) shadows = shadow_maker.get_shadow_pos(board, tetromino);
 
     for (int r = 2; r < 22; ++r) {
-        platform_renderer->set_cursor(BOARD_X, BOARD_Y + (r - 1));
+        platform_renderer->set_cursor(MY_BOARD_X, BOARD_Y + (r - 1));
 
         for (int c = 0; c < 10; ++c) {
             bool is_falling_block = false;
